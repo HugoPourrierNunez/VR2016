8921bbcbaf5bc1cb11479f60ed2d51770cdf97fb
HugoPourrierNunez <pourrier789@gmail.com>
2016-12-09 16:57:33 +0100

    Ajout du projet de test SteamVR

---
+++ TestSteamVR/Assets/Scripts/WebcamCubeScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebcamCubeScript : MonoBehaviour {
+
+    [SerializeField]
+    Renderer myRenderer;
+
+	// Use this for initialization
+	void Start () {
+        WebCamTexture webcamTexture = new WebCamTexture(WebCamTexture.devices[1].name);
+        myRenderer.material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+    }
+}
